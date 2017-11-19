809226f78cd86459734024992bdff276778250a2
Nathan <nathan@myeducator.com>
2017-11-19 16:41:36 -0700

    added initial models

--- project3/asset_management/models.py
+++ project3/asset_management/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    ''' A model that represents a location object '''
+    LOCATIONS = (
+        ('SJ', 'San Jose, CA, USA'),
+        ('LA', 'Los Angeles, CA, USA'),
+        ('D', 'Denver, CO, USA'),
+        ('M', 'Madrid, Spain'),
+        ('P', 'Paris, France'),
+        ('B', 'Beijing, China'),
+    )
+    location = models.TextField(blank=True, null=True, choices=LOCATIONS)
+
+    def __str__(self):
+        return '{}: {}'.format(self.pk, self.location)
+
+
+class Organization(models.Model):
+    ''' A model that represents a organization object '''
+    ORGANIZATION_NAMES = (
+        ('MO', 'MyOrg'),
+        ('EO', 'ExampleOrg'),
+    )
+    name = models.TextField(blank=True, null=True, choices=ORGANIZATION_NAMES)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}: {}, {}'.format(self.pk, self.name, self.description)
+
+
+class Manufacturer(models.Model):
+    ''' A model that represents a manufacturer object '''
+    MANUFACTURER_NAMES = (
+        ('D', 'Dell'),
+        ('A', 'Apple'),
+        ('T', 'Tesla'),
+        ('SX', 'SpaceX'),
+        ('M', 'Microsoft'),
+    )
+    name = models.TextField(blank=True, null=True, choices=MANUFACTURER_NAMES)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}: {}, {}'.format(self.pk, self.name, self.description)
+
+
+class Employee(models.Model):
+    ''' A model that represents a location object '''
+    ROLE_TYPES = (
+        ('A', 'Admin'),
+        ('M', 'Manager'),
+        ('E', 'Employee'),
+    )
+    name = models.TextField(blank=True, null=True)
+    role = models.TextField(blank=True, null=True, choices=ROLE_TYPES)
+
+    def __str__(self):
+        return '{}: {}, {}'.format(self.pk, self.name, self.role)
+
+class Asset(models.Model):
+    ''' A model that represents an asset object '''
+    ASSET_NAMES = (
+        ('C', 'Computer'),
+        ('P', 'Phone'),
+        ('S', 'Software'),
+        ('C', 'Car'),
+        ('R', 'Rocket'),
+    )
+    location = models.ForeignKey(Location, blank=True)
+    organization = models.ForeignKey(Organization, blank=True)
+    manufacturer = models.ForeignKey(Manufacturer, blank=True)
+    employee = models.ForeignKey(Employee, blank=True)
+    tag = models.CharField(blank=True, null=True, max_length=10)
+    name = models.TextField(blank=True, null=True, choices=ASSET_NAMES)
+    description = models.TextField(blank=True, null=True)
+    maintenance_notes = models.TextField(blank=True, null=True)
+    date_implemented = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}: {}, {}, {}, {}, {}'.format(self.pk, self.tag, self.name, self.description, self.maintenance_notes, self.date_implemented)

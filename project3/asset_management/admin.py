4045775836fa61cc2c44a060f7abc849e05b9d3f
Nathan <nathan@myeducator.com>
2017-11-19 16:50:15 -0700

    add access to admin site to manage objects

--- project3/asset_management/admin.py
+++ project3/asset_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Asset, Employee, Location, Manufacturer, Organization
+
+admin.site.register(Asset)
+admin.site.register(Employee)
+admin.site.register(Location)
+admin.site.register(Manufacturer)
+admin.site.register(Organization)

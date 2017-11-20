7f39686933ca97bab8faa6c3846f459ca09964bf
Nathan <nathan@myeducator.com>
2017-11-20 10:09:28 -0700

    fix admin so it works with new model asset name

--- project3/asset_management/admin.py
+++ project3/asset_management/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Asset, Employee, Location, Manufacturer, Organization
+from .models import (Asset, AssetName, Employee, Location, Manufacturer,
+                     Organization)
 
 admin.site.register(Asset)
 admin.site.register(Employee)
 admin.site.register(Location)
 admin.site.register(Manufacturer)
 admin.site.register(Organization)
+admin.site.register(AssetName)

af00e923149d7843fc1ac8046e3532c276533fba
root <root@ip-172-31-38-165.us-west-2.compute.internal>
2017-11-20 15:23:44 +0000

    set up django for server

--- project3/project3/settings.py
+++ project3/project3/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ebdb',
+            'USER': 'batchelb',
+            'PASSWORD': 'ISRocks!',
+            'HOST': 'aa19u69gsdxviyn.cswsr6wvrwv2.us-west-2.rds.amazonaws.com:5432',
+            'PORT': '5432',
+        }
 }
 
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")

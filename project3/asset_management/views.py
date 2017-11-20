bed1e8b23b58b226e70a7aa836dd8611463a2134
Nathan <nathan@myeducator.com>
2017-11-20 11:36:17 -0700

    working on search

--- project3/asset_management/views.py
+++ project3/asset_management/views.py
@@ -11,3 +11,14 @@ def index(request):
         'asset_list': asset_list,
     }
     return render(request, 'index.html', params)
+
+def search(request):
+    if request.method == 'POST':
+        asset_name =  request.POST.get('search')
+        try:
+            status = amod.Asset.objects.filter(name__icontains=asset_name)
+        except amod.Asset.DoesNotExist:
+            status = None
+        return render(request,"index.html",{"assets":status})
+    else:
+        return render(request,"index.html",{})
